12be2ffc2cf912c66f2f21425b9c41f9b9443f77
dxamima <95316551+dxamima@users.noreply.github.com>
2025-09-14 15:23:20 +0200

    initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import expressiveCode from 'astro-expressive-code';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+        expressiveCode({
+            themes: ['dark-plus'],
+        })
+    ],
+    vite: {
+        resolve: {
+            alias: {
+                '@': '/src'
+            }
+        }
+    },
+    adapter: cloudflare({
+        platformProxy: {
+            enabled: true
+        }
+    })
+});
