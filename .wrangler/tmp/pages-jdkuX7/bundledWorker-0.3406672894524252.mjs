ce3d4ee574387f6e2f45a4739c91989610513aae
dxamima <95316551+dxamima@users.noreply.github.com>
2025-09-14 15:34:27 +0200

    hhh

---
+++ .wrangler/tmp/pages-jdkuX7/bundledWorker-0.3406672894524252.mjs
@@ -0,0 +1,35 @@
+var __defProp = Object.defineProperty;
+var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
+
+// _worker.js/index.js
+import { renderers } from "./renderers.mjs";
+import { c as createExports, s as serverEntrypointModule } from "./chunks/_@astrojs-ssr-adapter_CPwSWTCx.mjs";
+import { manifest } from "./manifest_DOu4AJ0N.mjs";
+globalThis.process ??= {};
+globalThis.process.env ??= {};
+var serverIslandMap = /* @__PURE__ */ new Map();
+var _page0 = /* @__PURE__ */ __name(() => import("./pages/404.astro.mjs"), "_page0");
+var _page1 = /* @__PURE__ */ __name(() => import("./pages/index.astro.mjs"), "_page1");
+var pageMap = /* @__PURE__ */ new Map([
+  ["src/pages/404.astro", _page0],
+  ["src/pages/index.astro", _page1]
+]);
+var _manifest = Object.assign(manifest, {
+  pageMap,
+  serverIslandMap,
+  renderers,
+  actions: /* @__PURE__ */ __name(() => import("./_noop-actions.mjs"), "actions"),
+  middleware: /* @__PURE__ */ __name(() => import("./_astro-internal_middleware.mjs"), "middleware")
+});
+var _args = void 0;
+var _exports = createExports(_manifest);
+var __astrojsSsrVirtualEntry = _exports.default;
+var _start = "start";
+if (Object.prototype.hasOwnProperty.call(serverEntrypointModule, _start)) {
+  serverEntrypointModule[_start](_manifest, _args);
+}
+export {
+  __astrojsSsrVirtualEntry as default,
+  pageMap
+};
+//# sourceMappingURL=bundledWorker-0.3406672894524252.mjs.map
