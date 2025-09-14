e8517ec75d4a16d2a76b52c205355844f4108062
dxamima <95316551+dxamima@users.noreply.github.com>
2025-09-14 15:30:03 +0200

    h

---
+++ .wrangler/tmp/pages-YNg33Z/0o0vdtmngvf.js
@@ -0,0 +1,44 @@
+// <define:__ROUTES__>
+var define_ROUTES_default = {
+  version: 1,
+  include: [
+    "/_server-islands/*"
+  ],
+  exclude: [
+    "/",
+    "/_astro/*",
+    "/favicon.svg",
+    "/fonts/*",
+    "/404"
+  ]
+};
+
+// node_modules/.pnpm/wrangler@4.36.0_@cloudflare+workers-types@4.20250913.0/node_modules/wrangler/templates/pages-dev-pipeline.ts
+import worker from "/Users/davide/Desktop/mrmeowcat.dev/.wrangler/tmp/pages-YNg33Z/bundledWorker-0.047365951804198225.mjs";
+import { isRoutingRuleMatch } from "/Users/davide/Desktop/mrmeowcat.dev/node_modules/.pnpm/wrangler@4.36.0_@cloudflare+workers-types@4.20250913.0/node_modules/wrangler/templates/pages-dev-util.ts";
+export * from "/Users/davide/Desktop/mrmeowcat.dev/.wrangler/tmp/pages-YNg33Z/bundledWorker-0.047365951804198225.mjs";
+var routes = define_ROUTES_default;
+var pages_dev_pipeline_default = {
+  fetch(request, env, context) {
+    const { pathname } = new URL(request.url);
+    for (const exclude of routes.exclude) {
+      if (isRoutingRuleMatch(pathname, exclude)) {
+        return env.ASSETS.fetch(request);
+      }
+    }
+    for (const include of routes.include) {
+      if (isRoutingRuleMatch(pathname, include)) {
+        const workerAsHandler = worker;
+        if (workerAsHandler.fetch === void 0) {
+          throw new TypeError("Entry point missing `fetch` handler");
+        }
+        return workerAsHandler.fetch(request, env, context);
+      }
+    }
+    return env.ASSETS.fetch(request);
+  }
+};
+export {
+  pages_dev_pipeline_default as default
+};
+//# sourceMappingURL=0o0vdtmngvf.js.map
---
+++ .wrangler/tmp/pages-YNg33Z/manifest_Dos18AcG.mjs
@@ -0,0 +1,100 @@
+globalThis.process ??= {}; globalThis.process.env ??= {};
+import { g as decodeKey } from './chunks/astro/server_BWZSO3ey.mjs';
+import './chunks/astro-designed-error-pages_5JY9kFMV.mjs';
+import { N as NOOP_MIDDLEWARE_FN } from './chunks/noop-middleware_COk9tFP6.mjs';
+
+function sanitizeParams(params) {
+  return Object.fromEntries(
+    Object.entries(params).map(([key, value]) => {
+      if (typeof value === "string") {
+        return [key, value.normalize().replace(/#/g, "%23").replace(/\?/g, "%3F")];
+      }
+      return [key, value];
+    })
+  );
+}
+function getParameter(part, params) {
+  if (part.spread) {
+    return params[part.content.slice(3)] || "";
+  }
+  if (part.dynamic) {
+    if (!params[part.content]) {
+      throw new TypeError(`Missing parameter: ${part.content}`);
+    }
+    return params[part.content];
+  }
+  return part.content.normalize().replace(/\?/g, "%3F").replace(/#/g, "%23").replace(/%5B/g, "[").replace(/%5D/g, "]");
+}
+function getSegment(segment, params) {
+  const segmentPath = segment.map((part) => getParameter(part, params)).join("");
+  return segmentPath ? "/" + segmentPath : "";
+}
+function getRouteGenerator(segments, addTrailingSlash) {
+  return (params) => {
+    const sanitizedParams = sanitizeParams(params);
+    let trailing = "";
+    if (addTrailingSlash === "always" && segments.length) {
+      trailing = "/";
+    }
+    const path = segments.map((segment) => getSegment(segment, sanitizedParams)).join("") + trailing;
+    return path || "/";
+  };
+}
+
+function deserializeRouteData(rawRouteData) {
+  return {
+    route: rawRouteData.route,
+    type: rawRouteData.type,
+    pattern: new RegExp(rawRouteData.pattern),
+    params: rawRouteData.params,
+    component: rawRouteData.component,
+    generate: getRouteGenerator(rawRouteData.segments, rawRouteData._meta.trailingSlash),
+    pathname: rawRouteData.pathname || void 0,
+    segments: rawRouteData.segments,
+    prerender: rawRouteData.prerender,
+    redirect: rawRouteData.redirect,
+    redirectRoute: rawRouteData.redirectRoute ? deserializeRouteData(rawRouteData.redirectRoute) : void 0,
+    fallbackRoutes: rawRouteData.fallbackRoutes.map((fallback) => {
+      return deserializeRouteData(fallback);
+    }),
+    isIndex: rawRouteData.isIndex,
+    origin: rawRouteData.origin
+  };
+}
+
+function deserializeManifest(serializedManifest) {
+  const routes = [];
+  for (const serializedRoute of serializedManifest.routes) {
+    routes.push({
+      ...serializedRoute,
+      routeData: deserializeRouteData(serializedRoute.routeData)
+    });
+    const route = serializedRoute;
+    route.routeData = deserializeRouteData(serializedRoute.routeData);
+  }
+  const assets = new Set(serializedManifest.assets);
+  const componentMetadata = new Map(serializedManifest.componentMetadata);
+  const inlinedScripts = new Map(serializedManifest.inlinedScripts);
+  const clientDirectives = new Map(serializedManifest.clientDirectives);
+  const serverIslandNameMap = new Map(serializedManifest.serverIslandNameMap);
+  const key = decodeKey(serializedManifest.key);
+  return {
+    // in case user middleware exists, this no-op middleware will be reassigned (see plugin-ssr.ts)
+    middleware() {
+      return { onRequest: NOOP_MIDDLEWARE_FN };
+    },
+    ...serializedManifest,
+    assets,
+    componentMetadata,
+    inlinedScripts,
+    clientDirectives,
+    routes,
+    serverIslandNameMap,
+    key
+  };
+}
+
+const manifest = deserializeManifest({"hrefRoot":"file:///Users/davide/Desktop/mrmeowcat.dev/","cacheDir":"file:///Users/davide/Desktop/mrmeowcat.dev/node_modules/.astro/","outDir":"file:///Users/davide/Desktop/mrmeowcat.dev/dist/","srcDir":"file:///Users/davide/Desktop/mrmeowcat.dev/src/","publicDir":"file:///Users/davide/Desktop/mrmeowcat.dev/public/","buildClientDir":"file:///Users/davide/Desktop/mrmeowcat.dev/dist/","buildServerDir":"file:///Users/davide/Desktop/mrmeowcat.dev/dist/_worker.js/","adapterName":"@astrojs/cloudflare","routes":[{"file":"","links":[],"scripts":[],"styles":[],"routeData":{"type":"page","component":"_server-islands.astro","params":["name"],"segments":[[{"content":"_server-islands","dynamic":false,"spread":false}],[{"content":"name","dynamic":true,"spread":false}]],"pattern":"^\\/_server-islands\\/([^/]+?)\\/?$","prerender":false,"isIndex":false,"fallbackRoutes":[],"route":"/_server-islands/[name]","origin":"internal","_meta":{"trailingSlash":"ignore"}}},{"file":"404.html","links":[],"scripts":[],"styles":[],"routeData":{"route":"/404","isIndex":false,"type":"page","pattern":"^\\/404\\/?$","segments":[[{"content":"404","dynamic":false,"spread":false}]],"params":[],"component":"src/pages/404.astro","pathname":"/404","prerender":true,"fallbackRoutes":[],"distURL":[],"origin":"project","_meta":{"trailingSlash":"ignore"}}},{"file":"index.html","links":[],"scripts":[],"styles":[],"routeData":{"route":"/","isIndex":true,"type":"page","pattern":"^\\/$","segments":[],"params":[],"component":"src/pages/index.astro","pathname":"/","prerender":true,"fallbackRoutes":[],"distURL":[],"origin":"project","_meta":{"trailingSlash":"ignore"}}}],"base":"/","trailingSlash":"ignore","compressHTML":true,"componentMetadata":[["/Users/davide/Desktop/mrmeowcat.dev/src/pages/index.astro",{"propagation":"none","containsHead":true}],["/Users/davide/Desktop/mrmeowcat.dev/src/pages/404.astro",{"propagation":"none","containsHead":true}]],"renderers":[],"clientDirectives":[["idle","(()=>{var l=(n,t)=>{let i=async()=>{await(await n())()},e=typeof t.value==\"object\"?t.value:void 0,s={timeout:e==null?void 0:e.timeout};\"requestIdleCallback\"in window?window.requestIdleCallback(i,s):setTimeout(i,s.timeout||200)};(self.Astro||(self.Astro={})).idle=l;window.dispatchEvent(new Event(\"astro:idle\"));})();"],["load","(()=>{var e=async t=>{await(await t())()};(self.Astro||(self.Astro={})).load=e;window.dispatchEvent(new Event(\"astro:load\"));})();"],["media","(()=>{var n=(a,t)=>{let i=async()=>{await(await a())()};if(t.value){let e=matchMedia(t.value);e.matches?i():e.addEventListener(\"change\",i,{once:!0})}};(self.Astro||(self.Astro={})).media=n;window.dispatchEvent(new Event(\"astro:media\"));})();"],["only","(()=>{var e=async t=>{await(await t())()};(self.Astro||(self.Astro={})).only=e;window.dispatchEvent(new Event(\"astro:only\"));})();"],["visible","(()=>{var a=(s,i,o)=>{let r=async()=>{await(await s())()},t=typeof i.value==\"object\"?i.value:void 0,c={rootMargin:t==null?void 0:t.rootMargin},n=new IntersectionObserver(e=>{for(let l of e)if(l.isIntersecting){n.disconnect(),r();break}},c);for(let e of o.children)n.observe(e)};(self.Astro||(self.Astro={})).visible=a;window.dispatchEvent(new Event(\"astro:visible\"));})();"]],"entryModules":{"\u0000astro-internal:middleware":"_astro-internal_middleware.mjs","\u0000noop-actions":"_noop-actions.mjs","\u0000@astro-page:src/pages/404@_@astro":"pages/404.astro.mjs","\u0000@astro-page:src/pages/index@_@astro":"pages/index.astro.mjs","\u0000@astrojs-ssr-virtual-entry":"index.js","\u0000@astro-renderers":"renderers.mjs","\u0000@astrojs-ssr-adapter":"_@astrojs-ssr-adapter.mjs","\u0000@astrojs-manifest":"manifest_Dos18AcG.mjs","/Users/davide/Desktop/mrmeowcat.dev/node_modules/.pnpm/unstorage@1.17.1/node_modules/unstorage/drivers/cloudflare-kv-binding.mjs":"chunks/cloudflare-kv-binding_DMly_2Gl.mjs","astro:scripts/before-hydration.js":""},"inlinedScripts":[],"assets":["/_astro/ec.j8ru5.css","/_astro/ec.p1z7b.js","/favicon.svg","/_worker.js/_@astrojs-ssr-adapter.mjs","/_worker.js/_astro-internal_middleware.mjs","/_worker.js/_noop-actions.mjs","/_worker.js/index.js","/_worker.js/renderers.mjs","/fonts/DMSans.ttf","/fonts/Nunito-Italic.ttf","/fonts/Nunito.ttf","/_worker.js/_astro/ec.j8ru5.css","/_worker.js/_astro/ec.p1z7b.js","/_worker.js/chunks/LayoutPage_iV1FXxc6.mjs","/_worker.js/chunks/_@astrojs-ssr-adapter_CPwSWTCx.mjs","/_worker.js/chunks/astro-designed-error-pages_5JY9kFMV.mjs","/_worker.js/chunks/astro_CotbMU9I.mjs","/_worker.js/chunks/cloudflare-kv-binding_DMly_2Gl.mjs","/_worker.js/chunks/index_CP6Ed_uI.mjs","/_worker.js/chunks/noop-middleware_COk9tFP6.mjs","/_worker.js/pages/404.astro.mjs","/_worker.js/pages/index.astro.mjs","/_worker.js/chunks/astro/server_BWZSO3ey.mjs","/404.html","/index.html"],"buildFormat":"directory","checkOrigin":true,"serverIslandNameMap":[],"key":"FgKJJiopzLNmlRAB4XAhrqnwtIv1hnNWNB58bXrKe60=","sessionConfig":{"driver":"cloudflare-kv-binding","options":{"binding":"SESSION"}}});
+if (manifest.sessionConfig) manifest.sessionConfig.driverModule = () => import('./chunks/cloudflare-kv-binding_DMly_2Gl.mjs');
+
+export { manifest };
